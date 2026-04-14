c9723cb6bf8f0bb45a1f95c142c50a666b279ee4
krola1 <kwl082024@gmail.com>
2026-04-14 13:30:19 +0200

    created context

---
+++ src/context/TodoContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react";
+
+//Initialize context
+
+const TodoContext = createContext();
+
+//providerFunction
+export const TodoProvider = ({ children }) => {
+  //code for todolist here:
+  const [todos, setTodos] = useState([]);
+
+  //additem
+  //edititem
+  //removeitem
+
+  const providerObj = {
+    //insert values here
+    todos,
+  };
+  return (
+    <TodoContext.Provider value={providerObj}>{children}</TodoContext.Provider>
+  );
+};
+
+//custom hook
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTodo = () => {
+  const context = useContext(TodoContext);
+  if (!context) {
+    throw new Error("useContext must be used inside a provider");
+  }
+  return context;
+};
