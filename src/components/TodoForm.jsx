1ff4d8e7a54059dff40e9c8367aa94b0aa05fafa
krola1 <kwl082024@gmail.com>
2026-04-16 12:21:10 +0200

    m

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { useTodo } from "../context/TodoContext";
 export default function TodoForm() {
+  const { addItem } = useTodo();
+  const [text, setText] = useState("");
+
+  //handler function that uses additem to create a todo object based on text state.
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem(text);
+    setText("");
+  };
+
   return (
-    <form action="">
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <button>Add</button>
       <button>Delete todo</button>
     </form>
